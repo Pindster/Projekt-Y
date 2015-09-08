5e5cc30e5dbcc5ff269dcc2a51ecba249afd851b
Indro <indro.malleus@khk.ee>
2015-09-08 10:42:57 +0300

    1420600-102649904 Lisa avalehele logo done

--- index.php
+++ index.php
@@ -5,8 +5,74 @@
 </head>
 <body>
 
+<link href="css/styles.css" rel=“stylesheet”>
 
 <img src="assets/img/558520.png">
+
+<h1>Minu kontakt:</h1>
+ <p>Nimi: Rasmus Pind<br>
+    E-mail: rasmus.pind@khk.ee
+
+<h2>Antoropomeetria</h2>
+
+     <p>Minu pikkus: 187cm<br>
+        Minu kaal: 78kg</p>
+
+<h3>Enesetutvustus</h3>
+
+<h4>Haridustee</h4>
+
+<p>Tartu Tamme Gümnaasium 1999-2011<br>
+    Tartu Ülikool (bakalaureus) 2011-2014<br>
+    Tartu Ülikool (magister) 2014-...<br>
+    Tartu Kutsehariduskeskus 2015-...</p>
+
+
+<h5>Hobid</h5>
+<ul>
+    <li>
+    Sport
+    </li>
+
+<li>
+    Kalakasvatus
+</li>
+<li>
+    Kokkamine
+</li>
+</ul>
+
+
+
+<h6>Lemmikspordialad</h6>
+
+<table border="1">
+    <tr>
+        <td>NR#</td>
+        <td>Spordiala</td>
+        <td>Lemmiksportlane</td>
+    </tr>
+    <tr>
+        <td>1.</td>
+        <td>murdmaasuusatamine</td>
+        <td>Petter Northug</td>
+    </tr>
+    <tr>
+        <td>2.</td>
+        <td>Jooksmine</td>
+        <td>Mo Farah</td>
+    </tr>
+    <tr>
+        <td>3.</td>
+        <td>Jalgratas</td>
+        <td>Nairo Quintana</td>
+    </tr>
+    <tr>
+        <td>4.</td>
+        <td>Jalgpall</td>
+        <td>Wesley Sneijder</td>
+    </tr>
+</table>
 
 
 </body>
